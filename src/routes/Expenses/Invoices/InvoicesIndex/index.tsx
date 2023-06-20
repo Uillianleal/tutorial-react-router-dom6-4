471a3d414cc97eef81d2002622ea4f6a6ff9f57f
Uillian Leal <willianleal52@hotmail.com>
2023-06-20 11:08:55 -0300

    Index routes

---
+++ src/routes/Expenses/Invoices/InvoicesIndex/index.tsx
@@ -0,0 +1,9 @@
+function InvoiceIndex() {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <p>Select an invoice</p>
+    </main>
+  );
+}
+
+export default InvoiceIndex;
---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,9 @@
+function Welcome() {
+  return (
+    <main>
+      <p>Bem-vindos</p>
+    </main>
+  );
+}
+
+export default Welcome;
